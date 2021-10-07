267266c82ed3e5dc2a4866330036293324d5b18b
Nahuel <81537168+NahuelNancucheo@users.noreply.github.com>
2021-10-07 17:38:28 -0300

    Add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Board } from './components/Board/Board';
+import { Footer } from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>🧠 Sliding puzzle game 🧠</h1>
       <Board />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Footer.module.css'
+
+export const Footer = () => {
+    return (
+        <div className={styles.footer}>
+            <p>Made by <a href="https://github.com/NahuelNancucheo" rel="noreferrer" target="_blank">Nahuel Ñancucheo</a>🚀</p>
+        </div>
+    )
+}
